bb6b27af80bf0ab6e152e8e36c9eb12e0b005678
pinhead <o.cortesmauro@gmail.com>
2019-09-07 17:40:45 -0300

    Agrego modelo usuarios y ranking

--- escuadronvialapi/admin.py
+++ escuadronvialapi/admin.py
@@ -5,7 +5,11 @@ from django.contrib import admin
 from . models import Categoria
 from . models import Pregunta
 from . models import Respuesta
+from . models import Usuario
+from . models import Ranking
 
 admin.site.register(Categoria)
 admin.site.register(Pregunta)
 admin.site.register(Respuesta)
+admin.site.register(Usuario)
+admin.site.register(Ranking)
--- escuadronvialapi/models.py
+++ escuadronvialapi/models.py
@@ -5,11 +5,21 @@ class Categoria(models.Model):
 		
 class Pregunta(models.Model):
 	pregunta = models.CharField(max_length = 500)
-	categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+	categoria = models.ForeignKey(Categoria,on_delete=models.CASCADE)
 	estado = models.BooleanField(default=False)
 	
 
 class Respuesta(models.Model):
 	respuesta = models.CharField(max_length = 500)
 	correcta = models.BooleanField(default=False)
-	pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
+	pregunta = models.ForeignKey(Pregunta,on_delete=models.CASCADE)
+
+class Usuario(models.Model):
+	nombre = models.CharField(max_length = 50)
+	edad = models.IntegerField(default = 0)
+	email = models.CharField(max_length = 100)
+
+class Ranking(models.Model):
+	usuario = models.ForeignKey(Usuario,on_delete=models.CASCADE)
+	categoria = models.ForeignKey(Categoria,on_delete=models.CASCADE)
+	puntaje = models.IntegerField(default = 0)
