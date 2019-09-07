f1daca7d5233e526bd7c3d2b7907173fbdf1307c
Melissa1501 <msalazarbernabe@gmail.com>
2019-09-07 16:06:44 -0300

    agrego model de categorias

--- escuadronvial/settings.py
+++ escuadronvial/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'escuadronvialapi.apps.EscuadronvialapiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
