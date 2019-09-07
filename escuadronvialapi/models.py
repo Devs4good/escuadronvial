ed1808848ee2451f2c175847c1ef2f27d5a97902
Diego Cañizares <diegocanizares@gmail.com>
2019-09-07 17:46:35 -0300

    Agregando método __str__ en models

--- escuadronvialapi/models.py
+++ escuadronvialapi/models.py
@@ -1,25 +1,44 @@
 from django.db import models
 
+
 class Categoria(models.Model):
-	nombre = models.CharField(max_length = 100)
-		
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+
 class Pregunta(models.Model):
-	pregunta = models.CharField(max_length = 500)
-	categoria = models.ForeignKey(Categoria,on_delete=models.CASCADE)
-	estado = models.BooleanField(default=False)
-	
+    pregunta = models.CharField(max_length=500)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    estado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.pregunta
+
 
 class Respuesta(models.Model):
-	respuesta = models.CharField(max_length = 500)
-	correcta = models.BooleanField(default=False)
-	pregunta = models.ForeignKey(Pregunta,on_delete=models.CASCADE)
+    respuesta = models.CharField(max_length=500)
+    correcta = models.BooleanField(default=False)
+    pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.respuesta
+
 
 class Usuario(models.Model):
-	nombre = models.CharField(max_length = 50)
-	edad = models.IntegerField(default = 0)
-	email = models.CharField(max_length = 100)
+    nombre = models.CharField(max_length=50)
+    edad = models.IntegerField(default=0)
+    email = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
 
 class Ranking(models.Model):
-	usuario = models.ForeignKey(Usuario,on_delete=models.CASCADE)
-	categoria = models.ForeignKey(Categoria,on_delete=models.CASCADE)
-	puntaje = models.IntegerField(default = 0)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    puntaje = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.usuario
